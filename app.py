5b579a8d94c75d7fd8168a7fefefcf18a19ff357
gadhweyash <gadhweyash@gmail.com>
2024-08-27 20:18:42 +0530

    frontend 80per ML 0per backend 50per

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask,render_template,request
-
+import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/contact', methods = ["GET","POST"])
+def contactus():
+    if request.method == "POST":
+        fname = request.form.get("fullname")
+        pno = request.form.get("phone")
+        email = request.form.get("email")
+        addr = request.form.get("address")
+        msg = request.form.get("message")
+        # print(fname,pno,email,addr,msg)
+        conn = sqlite3.connect("ytdatabase.db")
+        cur = conn.cursor()
+        cur.execute(f'''
+                    INSERT INTO CONTACT VALUES("{fname}","{pno}","{email}","{addr}","{msg}")    
+                ''')
+        conn.commit()
+        return render_template('successfull.html')
+    else:
+        return render_template('contactus.html')
+    # pass
+
+@app.route('/likepredict')
+def likepredict():
+    return render_template('likepredict.html')
 
 if __name__ == '__main__':
     app.run(port=1010)
